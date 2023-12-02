622d73d0b3a9d0fe600616df43e5c8f437a33711
valentin <valentin.rogister@gmail.com>
2023-12-02 22:39:15 +0100

    day02 part1

---
+++ day02/part1.ex
@@ -0,0 +1,40 @@
+defmodule M do
+  defp valid(["Game", _ | tail]) do
+    valid(tail)
+  end
+
+  defp valid([a, b | tail]) do
+    {n, ""} = Integer.parse(a)
+
+    max =
+      case b do
+        "red" -> 12
+        "green" -> 13
+        "blue" -> 14
+      end
+
+    n <= max and valid(tail)
+  end
+
+  defp valid([]) do
+    true
+  end
+
+  def solve do
+    IO.read(:stdio, :all)
+    |> String.trim()
+    |> String.replace(":", "")
+    |> String.replace(";", "")
+    |> String.replace(",", "")
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1))
+    |> Enum.filter(&valid(&1))
+    |> Enum.map(fn [_, id | _] ->
+      {id, ""} = Integer.parse(id)
+      id
+    end)
+    |> Enum.sum()
+  end
+end
+
+IO.puts(M.solve())
