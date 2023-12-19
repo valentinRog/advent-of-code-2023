545c1f735e31fd0eb74d4dc889a10442fe900581
valentin <valentin.rogister@gmail.com>
2023-12-20 00:35:24 +0100

    memoization

--- day04/part2.ex
+++ day04/part2.ex
@@ -1,12 +1,28 @@
 defmodule M do
-  def score(m, i) do
-    m |> Map.get(i)
+  use Agent
 
-    1 +
-      case m |> Map.get(i, 0) do
-        0 -> 0
-        n -> (i + 1)..(i + n) |> Stream.map(&score(m, &1)) |> Enum.sum()
-      end
+  defp get_from_cache(i) do
+    Agent.get(__MODULE__, & &1)[i]
+  end
+
+  defp add_to_cache(i, n) do
+    Agent.update(__MODULE__, fn cache -> cache |> Map.put(i, n) end)
+  end
+
+  defp score(m, i) do
+    case get_from_cache(i) do
+      nil ->
+        add_to_cache(
+          i,
+          1 +
+            case m |> Map.get(i, 0) do
+              0 -> 0
+              n -> (i + 1)..(i + n) |> Stream.map(&score(m, &1)) |> Enum.sum()
+            end
+        )
+        score(m, i)
+      n -> n
+    end
   end
 
   def solve do
@@ -26,6 +42,7 @@ defmodule M do
       |> Enum.with_index()
       |> Enum.reduce(%{}, fn {v, k}, acc -> acc |> Map.put(k, v) end)
 
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
     m |> Map.keys() |> Stream.map(&score(m, &1)) |> Enum.sum()
   end
 end
