af1202c27ee7cb873c20c3508ed38009976394d9
valentin <valentin.rogister@gmail.com>
2024-02-04 13:48:21 +0100

    better day18 part1

--- day18/part1.ex
+++ day18/part1.ex
@@ -1,45 +1,50 @@
 defmodule P do
   def add({px1, py1}, {px2, py2}), do: {px1 + px2, py1 + py2}
+
+  def prod({px, py}, k), do: {px * k, py * k}
+
+  def module({px, py}), do: abs(px) + abs(py)
+
+  def manhattan(p1, p2), do: module(p2 |> add(p1 |> prod(-1)))
 end
 
 defmodule M do
-  defp compute(m, p, cache) do
-    cond do
-      cache |> MapSet.member?(p) ->
-        MapSet.new()
+  defp shoelace(l) do
+    mx = l |> Enum.reduce(%{}, fn {x, _}, m -> m |> Map.put(m |> map_size(), x) end)
+    my = l |> Enum.reduce(%{}, fn {_, y}, m -> m |> Map.put(m |> map_size(), y) end)
+    l = length(l)
 
-      m |> MapSet.member?(p) ->
-        MapSet.new()
-
-      true ->
-        [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
-        |> Enum.reduce(cache |> MapSet.put(p), fn d, cache ->
-          cache |> MapSet.union(compute(m, p |> P.add(d), cache))
-        end)
-    end
+    0..(l - 1)
+    |> Enum.reduce(0, fn i, acc ->
+      acc + mx[i] * my[rem(i + 1, l)] - mx[rem(i + 1, l)] * my[i]
+    end)
+    |> div(2)
   end
 
-  defp compute(m), do: compute(m, {1, 1}, MapSet.new())
+  defp perimeter([], _, n), do: n
+
+  defp perimeter([h | t], p, n) do
+    perimeter(t, h, n + P.manhattan(p, h))
+  end
+
+  defp perimeter(l), do: perimeter(l, {0, 0}, 0)
 
   def solve do
     dirs = %{"U" => {0, -1}, "D" => {0, 1}, "L" => {-1, 0}, "R" => {1, 0}}
 
-    m =
+    l =
       IO.read(:all)
       |> String.trim()
       |> String.split("\n")
       |> Enum.map(&String.split/1)
       |> Enum.map(fn [d, n, _] -> {d, n |> String.to_integer()} end)
-      |> Enum.reduce({{0, 0}, MapSet.new()}, fn {d, n}, {p, acc} ->
-        1..n
-        |> Enum.reduce({p, acc}, fn _, {p, acc} ->
-          np = p |> P.add(dirs[d])
-          {np, acc |> MapSet.put(np)}
-        end)
+      |> Enum.reduce({{0, 0}, []}, fn {d, n}, {p, l} ->
+        p = p |> P.add(dirs[d] |> P.prod(n))
+        {p, l ++ [p]}
       end)
       |> elem(1)
 
-    (compute(m) |> MapSet.size()) + (m |> MapSet.size())
+    shoelace(l) + (perimeter(l) |> div(2)) + 1
   end
 end
 
