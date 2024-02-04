8cd9000b27958bca86e493e777638733ad732da0
valentin <valentin.rogister@gmail.com>
2024-02-04 14:00:22 +0100

    day18 (cool)

---
+++ day18/part2.ex
@@ -0,0 +1,56 @@
+defmodule P do
+  def add({px1, py1}, {px2, py2}), do: {px1 + px2, py1 + py2}
+
+  def prod({px, py}, k), do: {px * k, py * k}
+
+  def module({px, py}), do: abs(px) + abs(py)
+
+  def manhattan(p1, p2), do: module(p2 |> add(p1 |> prod(-1)))
+end
+
+defmodule M do
+  defp shoelace(l) do
+    mx = l |> Enum.reduce(%{}, fn {x, _}, m -> m |> Map.put(m |> map_size(), x) end)
+    my = l |> Enum.reduce(%{}, fn {_, y}, m -> m |> Map.put(m |> map_size(), y) end)
+    l = length(l)
+
+    0..(l - 1)
+    |> Enum.reduce(0, fn i, acc ->
+      acc + mx[i] * my[rem(i + 1, l)] - mx[rem(i + 1, l)] * my[i]
+    end)
+    |> div(2)
+  end
+
+  defp perimeter([], _, n), do: n
+
+  defp perimeter([h | t], p, n) do
+    perimeter(t, h, n + P.manhattan(p, h))
+  end
+
+  defp perimeter(l), do: perimeter(l, {0, 0}, 0)
+
+  def solve do
+    dirs = %{"3" => {0, -1}, "1" => {0, 1}, "2" => {-1, 0}, "0" => {1, 0}}
+
+    l =
+      IO.read(:all)
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.map(&String.split/1)
+      |> Enum.map(fn [_, _, hex] -> hex |> String.slice(2, (hex |> String.length()) - 3) end)
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.map(&Enum.reverse/1)
+      |> Enum.map(fn [h | t] ->
+        {h, t |> Enum.reverse() |> Enum.join() |> String.to_integer(16)}
+      end)
+      |> Enum.reduce({{0, 0}, []}, fn {d, n}, {p, l} ->
+        p = p |> P.add(dirs[d] |> P.prod(n))
+        {p, l ++ [p]}
+      end)
+      |> elem(1)
+
+    shoelace(l) + (perimeter(l) |> div(2)) + 1
+  end
+end
+
+M.solve() |> IO.puts()
