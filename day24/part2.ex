f4ed05fed113580f47965aa3ddbcb10c0a9088be
Valentin <valentin.rogister@gmail.com>
2024-02-21 20:29:21 +0100

    fixed floating point issue

--- day24/part2.ex
+++ day24/part2.ex
@@ -3,10 +3,10 @@ defmodule M do
     [
       vy0 - vyi,
       vxi - vx0,
-      0.0,
+      0,
       pyi - py0,
       px0 - pxi,
-      0.0,
+      0,
       px0 * vy0 - py0 * vx0 - pxi * vyi + pyi * vxi
     ]
   end
@@ -14,10 +14,10 @@ defmodule M do
   defp e2([px0, _py0, pz0, vx0, _vy0, vz0], [pxi, _pyi, pzi, vxi, _vyi, vzi]) do
     [
       vz0 - vzi,
-      0.0,
+      0,
       vxi - vx0,
       pzi - pz0,
-      0.0,
+      0,
       px0 - pxi,
       px0 * vz0 - pz0 * vx0 - pxi * vzi + pzi * vxi
     ]
@@ -78,7 +78,13 @@ defmodule M do
   defp det(m) do
     0..mi(m)
     |> Enum.reduce(0, fn i, acc ->
-      acc + :math.pow(-1, i) * m[{i, 0}] * det(submatrix(m, {i, 0}))
+      sign =
+        case rem(i, 2) do
+          0 -> 1
+          1 -> -1
+        end
+
+      acc + sign * m[{i, 0}] * det(submatrix(m, {i, 0}))
     end)
   end
 
@@ -86,18 +92,9 @@ defmodule M do
     l |> Enum.with_index() |> Enum.reduce(m, fn {n, i}, acc -> acc |> Map.put({i, j}, n) end)
   end
 
-  defp krammer(a, b) do
+  defp cramer(a, b) do
     d = det(a)
-    0..2 |> Enum.map(fn j -> (a |> replace_col(b, j) |> det()) / d end)
-  end
-
-  defp compute(data) do
-    r0 = data |> Enum.random()
-    rs = data |> Enum.filter(&(&1 != r0)) |> Enum.take_random(3)
-    coeff = rs |> Enum.flat_map(fn r -> [e1(r0, r), e2(r0, r)] end)
-    a = coeff |> Enum.map(&List.delete_at(&1, -1)) |> make_matrix()
-    b = coeff |> Enum.map(&Enum.at(&1, -1))
-    krammer(a, b) |> Enum.sum()
+    0..2 |> Enum.map(fn j -> div(a |> replace_col(b, j) |> det(), d) end)
   end
 
   def solve do
@@ -109,8 +106,12 @@ defmodule M do
       |> String.split("\n")
       |> Enum.map(fn s -> s |> String.split() |> Enum.map(&String.to_integer/1) end)
 
-    freq = 1..1000 |> Stream.map(fn _ -> compute(data) end) |> Enum.frequencies()
-    freq |> Map.keys() |> Enum.max_by(&freq[&1]) |> round()
+    r0 = data |> Enum.at(1)
+    rs = data |> Enum.drop(1) |> Enum.take_random(3)
+    coeff = rs |> Enum.flat_map(fn r -> [e1(r0, r), e2(r0, r)] end)
+    a = coeff |> Enum.map(&List.delete_at(&1, -1)) |> make_matrix()
+    b = coeff |> Enum.map(&Enum.at(&1, -1))
+    cramer(a, b) |> Enum.sum()
   end
 end
 
